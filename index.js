820ad603ce1a869e7c9230f1634e27f2cb1092c6
Leonid <Chunl3e666@yandex.ru>
2022-10-07 16:29:12 +0300

    Feat: Учим бота быть немного вежлевее

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
     const text = msg.text;
     if (text === '/start') {
-        await bot.sendMessage(chatId, 'Добры день, пидор! Если есть проблемы с подбором товаров то жми кнопку снизу, мы поможем.', { 
+        await bot.sendMessage(chatId, 'Добры день! Если есть проблемы с подбором товаров то жми кнопку снизу, мы поможем.', { 
             reply_markup: {
                 inline_keyboard: [
-                    [{ text: 'Я тупой', web_app:{url:webAppUrl} }]
+                    [{ text: 'Помогите мне!', web_app:{url:webAppUrl} }]
                 ]
             }
         });
